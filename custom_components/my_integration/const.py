7bd94be2d6f07d6db5b09b26328a782ba302073e
PCgo <58768472+PCgoDK@users.noreply.github.com>
2026-06-11 15:38:52 +0200

    Rename integration display name to Kronprins Frederiks Bro

--- custom_components/my_integration/const.py
+++ custom_components/my_integration/const.py
@@ -3,6 +3,6 @@
 DOMAIN = "my_integration"
 CONF_HOST = "host"
 CONF_NAME = "name"
-DEFAULT_NAME = "My Integration"
+DEFAULT_NAME = "Kronprins Frederiks Bro"
 PLATFORMS = ["sensor"]
 COORDINATOR = "coordinator"
