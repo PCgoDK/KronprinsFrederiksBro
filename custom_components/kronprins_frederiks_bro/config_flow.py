caacc30b498c30de1fa3d5ab8476acd4f815a6bf
PCgo <58768472+PCgoDK@users.noreply.github.com>
2026-06-12 12:43:01 +0200

    Fix Home Assistant options flow setup

--- custom_components/kronprins_frederiks_bro/config_flow.py
+++ custom_components/kronprins_frederiks_bro/config_flow.py
@@ -41,14 +41,11 @@ class MyIntegrationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @callback
     def async_get_options_flow(config_entry):
         """Create the options flow."""
-        return MyIntegrationOptionsFlow(config_entry)
+        return MyIntegrationOptionsFlow()
 
 
 class MyIntegrationOptionsFlow(config_entries.OptionsFlow):
     """Handle My Integration options."""
-
-    def __init__(self, config_entry) -> None:
-        self.config_entry = config_entry
 
     async def async_step_init(self, user_input: dict | None = None):
         """Manage integration options."""
